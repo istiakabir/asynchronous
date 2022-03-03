12c435f6ece900cb14fc8c80fb3b45d95d701bc3
istiakabir <istiakabirtrofder@gmail.com>
2022-03-03 17:44:21 +0600

    first commit

---
+++ asynchronous.js
@@ -0,0 +1,9 @@
+function doSomehing() {
+    console.log('Here is timer')
+};
+
+console.log('First : Ami first hoye gelam');
+console.log('Second : Ami first hoye gelam');
+setTimeout(doSomehing, 5000);
+console.log('Third : Ami first hoye gelam');
+console.log('Four : Ami first hoye gelam');
